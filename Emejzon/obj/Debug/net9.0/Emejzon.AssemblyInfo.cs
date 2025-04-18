41720acaf7c699bb0c5248ad34a2ab662bdb5766
Filip Radke <129221950+filipRadke@users.noreply.github.com>
2025-04-18 23:46:07 +0200

    Authored ExampleValues.sql

--- Emejzon/obj/Debug/net9.0/Emejzon.AssemblyInfo.cs
+++ Emejzon/obj/Debug/net9.0/Emejzon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Emejzon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b7bfdee75f57676939dbb3d43020b619ac814c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb468a503bf2e67d66968b86256ae7adb90909af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Emejzon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Emejzon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
